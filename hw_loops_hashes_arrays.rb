549244fe16a599323523a019a20c55b2dcf5f442
Gary Muir <garymuir17@gmail.com>
2018-05-23 16:54:36 +0100

    Q 6 and 7 of Ex B done

--- hw_loops_hashes_arrays.rb
+++ hw_loops_hashes_arrays.rb
@@ -109,8 +109,18 @@ users["Erik"][:lottery_numbers]
 users["Avril"][:pets][0][:species]
 users["Avril"][:pets][0][:species].class()
 #5. Get the smallest of Erik's lottery numbers
+users["Erik"][:lottery_numbers].sort().slice(0)
 #6. Return an array of Avril's lottery numbers that are even
+new_array = []
+ for number in users["Avril"][:lottery_numbers]
+   case number % 2
+   when 0
+     new_array.push(number)
+   end
+ end
+# p new_array
 #7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
+users["Erik"][:lottery_numbers].push(7)
 #8. Change Erik's hometown to Edinburgh
 #9. Add a pet dog to Erik called "Fluffy"
 #10. Add another person to the users hash
