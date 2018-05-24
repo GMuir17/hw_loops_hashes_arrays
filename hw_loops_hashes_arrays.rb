987312e0c1f9d81444f89fedebaaa2895e36fd01
Gary Muir <garymuir17@gmail.com>
2018-05-24 09:52:25 +0100

    cleaned Ex A, Q 8

--- hw_loops_hashes_arrays.rb
+++ hw_loops_hashes_arrays.rb
@@ -28,7 +28,6 @@ stops.length()
 #8. How many ways can we return `"Falkirk High"` from the array?
 # return stops[2]
 # return stops.at(2)
-# return stops[2]
 # return stops.fetch(2)
 #9. Reverse the positions of the stops in the array
 # stops.reverse()
